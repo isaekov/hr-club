2b382b1252bd68378a6a0e5ffb70efff2e1441d0
ildar <derorn@mail.ru>
2021-09-10 09:03:06 +0300

    загрузка файла интерфейс

--- src/main/java/ru/hwru/integration/controllers/gallery/GalleryController.java
+++ src/main/java/ru/hwru/integration/controllers/gallery/GalleryController.java
@@ -1,15 +1,31 @@
 package ru.hwru.integration.controllers.gallery;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.hwru.integration.service.upload.FileStorageService;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
 
+    private final FileStorageService fileStorageService;
+
+    public GalleryController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
     @GetMapping
     public String gallery() {
         return "gallery/index";
+    }
+
+
+    @GetMapping("/list")
+    public String list(Model model) {
+
+        model.addAttribute("fileLists", fileStorageService.loadF());
+        return "gallery/list";
     }
 }
